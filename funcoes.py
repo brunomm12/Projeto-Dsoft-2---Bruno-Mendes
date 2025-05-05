7f2fe7f1cfd23251791860944b510d43581471ef
Bruno <brunomm12@al.insper.edu.br>
2025-05-05 13:51:10 -0300

    funções 1

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,12 @@
+import random
 
 
-teste2 = 'oi'
+def rolar_dados(n):
+    rolados = []
+
+    for i in range(n):
+
+        n_randomico = random.randint(1,6)
+        rolados.append(n_randomico)
+
+    return rolados
